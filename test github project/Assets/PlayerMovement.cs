8ffc4ce9fb91cb6146d2b73fef3daba03059c029
Emby <99022462+Emby01@users.noreply.github.com>
2022-09-15 18:19:57 +1000

    PT3 of series done
    thanks brackeys

--- test github project/Assets/PlayerMovement.cs
+++ test github project/Assets/PlayerMovement.cs
@@ -4,9 +4,24 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb; // Reference to Rigidbody component, called "rb"
 
+    public float forwardForce = 2000f; // Variable for the forward force on the cube
+    public float sidewaysForce = 500f; // Same but sideways
+
     // Use FixedUpdate instead of Update when messing around with physics, apparently
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime); // Adds a force of 2000 on the z-axis
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); // Adds a forward force on the z-axis
+
+        if(Input.GetKey("d"))
+        {
+            // Ifs only executed if condition is met
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if(Input.GetKey("a"))
+        {
+            // Ifs only executed if condition is met
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
